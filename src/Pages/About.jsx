ebd3d138887a6953423c60246bcd557c20011dd0
Yassir Abidy <yassir.abidy@gmail.com>
2025-01-11 22:18:34 +0100

    My instead of yassir's

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -224,7 +224,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/drive/folders/1BOm51Grsabb3zj6Xk27K-iRwI1zITcpo" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1C1S2UF5cLWekBN2eA9yKgLFn52YdsTMW" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
