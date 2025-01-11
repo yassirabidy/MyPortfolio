e75daf2b741fc397077f057ca7d61a8718077cc4
Yassir Abidy <yassir.abidy@gmail.com>
2025-01-11 21:36:39 +0100

    Replaced react-swipeable-views with react-swipeable

--- src/App.jsx
+++ src/App.jsx
@@ -55,9 +55,9 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2025{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            Portfolio
+            Yassir ABIDY
           </a>
           . All Rights Reserved.
         </span>
